5ad806b52f7e1ee55d0048e50c5711efef50103f
Erick Kelvin <erickkelvin@Ericks-MacBook-Pro.local>
2017-10-08 23:14:11 -0300

    new menu version

--- js/script.js
+++ js/script.js
@@ -1,14 +1,34 @@
-var href = "";
 $(document).ready(function() {
-    $("a").on("click",function(e){
-        href = $(this).attr('href');
-        if (href.includes("mailto")) return;
-        e.preventDefault();
-        $("body").animate({opacity:"0", "margin-top": "-2%" }, 200, function() {
-            document.location = href;
-            $("body").css("opacity", "1");
-            $("body").css("margin-top", "0px");
-        });
-        return false;
-    })
+    let currPage = window.location.href.split("#");
+    if (currPage.length > 1) {
+        let currID = "#" + currPage[1];
+        $(currID).slideDown();
+        $("#header").css("height","100px");
+        $('html, body').animate({
+            scrollTop: ($(currID).offset().top)
+        },700);
+        $("a[href='" + currID + "']").addClass("curr-item");
+        
+    }
+
+    $(".menu-item").click(function() {
+        let item = $($(this).attr("href"));
+        if (item.is(":visible")) {
+            $(this).removeClass("curr-item");
+            item.slideUp();
+            $("html, body").animate({ scrollTop: "0" });
+            $("#header").css("height","150px");
+        }
+        else {
+            $(".menu-item").removeClass("curr-item");
+            $(this).addClass("curr-item");
+            $(".content").slideUp();
+            item.slideDown();
+            $('html, body').animate({
+                scrollTop: (item.offset().top - $("body").offset().top + $("body").scrollTop())
+            },700);
+            $("#header").css("height","100px");
+        }
+    });
+
 });

4ce86c6c37a41c7826399f5f4e502f1ae27d0c25
Erick Kelvin <erickkelvin@hotmail.com>
2017-12-21 01:01:30 -0300

    fixing firefox bug that prevented page switching

--- js/script.js
+++ js/script.js
@@ -1,38 +1,54 @@
 $(document).ready(function() {
+
+    loadPage();
+
+    window.onpopstate = function () { switchPage(); };
+
+    $(".menu-item").click(function(e) {
+        if ($(this).attr("href") == window.location.hash) {
+            e.preventDefault();
+            switchPage();
+        }
+    });
+
+});
+
+function loadPage() {
     var currPage = window.location.href.split("#");
     if ((currPage.length > 1)&&(currPage[1]!="")) {
         var currID = "#" + currPage[1];
         if ($(currID).length) {
-            showContent($("a[href='" + currID + "']"),$(currID));
+            showTab($("a[href='" + currID + "']"),$(currID));
         }
     }
+}
 
-    window.onpopstate = function() {
-        var currMenuTitle = window.location.hash.charAt(1).toUpperCase() + window.location.hash.slice(2);
-        var currMenuItem = $(".menu-item:contains('"+currMenuTitle+"')");
-        var currItemContent = $(window.location.hash);
-
-        if ((currItemContent.is(":visible"))||(window.location.hash=="")) {
-            currMenuItem.removeClass("curr-item");
-            $(".content").slideUp();
+function switchPage() {
+    var currMenuTitle = window.location.hash.charAt(1).toUpperCase() + window.location.hash.slice(2);
+    var currMenuItem = $(".menu-item:contains('"+currMenuTitle+"')");
+    var currItemContent = $(window.location.hash);
+    console.log(window.location.hash);
+    if ((currItemContent.is(":visible"))||(window.location.hash=="")) {
+        currMenuItem.removeClass("curr-item");
+        $(document).scrollTop(0);
+        $(".content").slideUp(function() {
             $("html, body").animate({ scrollTop: "0" });
-            $("#header").css("height","130px");
-            if($(window).width() > 767) { $("hr").css("margin", "60px auto"); }
-            else { $("hr").css("margin", "35px auto"); }
-            document.title = "Erick Santos";
-            if (currItemContent.is(":visible")) {
-                history.pushState("", "Erick Santos", window.location.pathname);
-            }
+        });
+        $("#header").css("height","130px");
+        if($(window).width() > 767) { $("hr").css("margin", "60px auto"); }
+        else { $("hr").css("margin", "35px auto"); }
+        document.title = "Erick Santos";
+        if (currItemContent.is(":visible")) {
+            history.pushState("", "Erick Santos", window.location.pathname);
         }
-        else {
-            $(".menu-item").removeClass("curr-item");
-            showContent(currMenuItem,currItemContent);
-        }
-    };
+    }
+    else {
+        $(".menu-item").removeClass("curr-item");
+        showTab(currMenuItem,currItemContent);
+    }
+}
 
-});
-
-function showContent(menuItem, item) {
+function showTab(menuItem, item) {
     menuItem.addClass("curr-item");
     if ($(".content").is(":visible")) {
         $(".content").slideUp(function(){
@@ -43,9 +59,7 @@ function showContent(menuItem, item) {
         item.slideDown();
     }
     document.title = menuItem.text() + " | Erick Santos";
-    $('html, body').animate({
-        scrollTop: item.offset().top - 100
-    });
+
     $("#header").css("height","90px");
-    $("hr").css("margin", "30px auto");
+    $("hr").css("margin", "25px auto");
 }

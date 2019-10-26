4bebed84065c55ab527f0c326a65a5b1445247a1
Erick Kelvin <erickkelvin@hotmail.com>
2019-10-25 21:50:11 -0300

    Updates

---
+++ precache-manifest.ecf6b4995010fc8f0522bf0f2ffdc846.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca4a1857a1f2ce3d9611119f00dcd8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c207ce77bff8ef9a017",
+    "url": "/static/css/main.23e83957.chunk.css"
+  },
+  {
+    "revision": "656d1e630406ed681914",
+    "url": "/static/js/2.2b9eaa0d.chunk.js"
+  },
+  {
+    "revision": "8c207ce77bff8ef9a017",
+    "url": "/static/js/main.ae774fc3.chunk.js"
+  },
+  {
+    "revision": "db6dae3598b8454cf3c4",
+    "url": "/static/js/runtime-main.7d778090.js"
+  },
+  {
+    "revision": "272bf9bb9e610fb2a0f9275cb7a8f6df",
+    "url": "/static/media/avenir-next-regular.272bf9bb.woff"
+  },
+  {
+    "revision": "7598590a51b809d5aa7cb170cb1b4a8a",
+    "url": "/static/media/profile.7598590a.jpg"
+  }
+]);
